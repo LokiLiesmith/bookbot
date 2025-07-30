78d4229c3d4940be56166b9a2980155143aa9a40
LokiLiesmith <mark.razem@gmail.com>
2025-07-30 20:46:42 +0200

    added books, takes argv path, final commit

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-from stats import count_words
+import sys
+from stats import count_words, count_characters, expand_dict
 
 def	main():
-	book_path = "books/frankenstein.txt"
+	if len(sys.argv) != 2:
+		print("Usage: python3 main.py <path_to_book>")
+	book_path = sys.argv[1]
 	text = get_book_text(book_path)
 	num_words = count_words(text)
-	print(f"{num_words} words found in the document")
+	num_chars = count_characters(text)
+	sorted_list = expand_dict(num_chars)
+	# print(f"{num_words} words found in the document")
+	# print(f"{num_chars}")
+	print("============ BOOKBOT ============")
+	print(f"Analyzing book found at {book_path}...")
+	print("----------- Word Count ----------")
+	print(f"Found {num_words} total words")
+	print("--------- Character Count -------")
+	for i in sorted_list:
+		if i["char"].isalpha():
+			print(f"{i["char"]}: {i["num"]}")
+	print("============= END ===============")
 
 def get_book_text(path):
     with open(path) as f:
--- stats.py
+++ stats.py
@@ -1,3 +1,22 @@
 def count_words(text):
 	num_words = len(text.split())
 	return num_words
+
+def count_characters(text):
+	lower_case = text.lower()
+	result = {}
+	for i in sorted(set(lower_case)):
+		result[i] = lower_case.count(i)
+	return result
+
+def sort_on(items):
+	# print(f"{items["num"]}")
+	return items["num"]
+
+def expand_dict(dict):
+	list = []
+	for i in dict:
+		smol = {"char" : i, "num" : dict[i]}
+		list.append(smol)
+	list.sort(reverse=True, key=sort_on)
+	return (list)
